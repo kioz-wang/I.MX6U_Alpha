608fc0b40756e3e81a95394be7698b4339b06c4a
kioz.wang <never.had@outlook.com>
2025-07-11 02:15:48 +0800

    Bugfix [env:buildenv] 修复中文乱码的问题；`setup.sh` 新增 `run`, `run_once` 命令

--- env/setup.sh
+++ env/setup.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2155
+declare -r P_ENV=$(realpath "${BASH_SOURCE[0]%/*}")
+declare -r P_ROOT=$(dirname "${P_ENV}")
+declare -r N_ROOT=$(basename "${P_ROOT}")
+
 function build() {
-    podman build --http-proxy=false --build-arg-file argfile.conf --tag kioz0wang/buildenv:1.0 -f Containerfile.buildenv "$@"
+    podman build --http-proxy=false --build-arg-file "${P_ENV}"/argfile.conf --tag kioz0wang/buildenv:1.0 -f "${P_ENV}"/Containerfile.buildenv
 }
 
 function create() {
-    podman container create --http-proxy=false -it --publish 10022:10022 localhost/kioz0wang/buildenv:1.0 --name buildenv "$@"
-    podman container start buildenv
+    podman container create --http-proxy=false --interactive --tty --publish 10022:10022 --volume "${P_ROOT}":"/root/${N_ROOT}" --name buildenv localhost/kioz0wang/buildenv:1.0
+}
+
+function run() {
+    podman container start buildenv || return $?
+    echo "=> login root with password [123]"
+    echo "  ssh -o StrictHostKeyChecking=no -p 10022 root@localhost"
+}
+
+function run_once() {
+    podman run --rm --http-proxy=false --interactive --tty --publish 10022:10022 --volume "${P_ROOT}":"/root/${N_ROOT}" localhost/kioz0wang/buildenv:1.0
 }
 
 function main() {
