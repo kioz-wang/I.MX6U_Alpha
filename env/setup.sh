a5b676967bd2a8170f5db5decf950222110fe83b
kioz.wang <never.had@outlook.com>
2025-07-13 13:15:34 +0800

    Optimize [env] 调整组织结构；精简`setup`；新增`im6ull_sdk`镜像

--- env/setup.sh
+++ env/setup.sh
@@ -5,34 +5,83 @@ declare -r P_ENV=$(realpath "${BASH_SOURCE[0]%/*}")
 declare -r P_ROOT=$(dirname "${P_ENV}")
 declare -r N_ROOT=$(basename "${P_ROOT}")
 
+declare g_target
+declare g_tag
+
 function build() {
-    podman build --http-proxy=false --build-arg-file "${P_ENV}"/argfile.conf --tag kioz0wang/buildenv:1.0 -f "${P_ENV}"/Containerfile
+    if [[ ${g_tag} == "latest" ]]; then
+        echo "Don't use tag 'latest'"
+        return 1
+    fi
+
+    if [ -f "${P_ENV}/${g_target}/.pre" ]; then
+        bash "${P_ENV}/${g_target}/.pre" "${P_ROOT}" || return $?
+    fi
+
+    podman build --http-proxy=false --build-arg-file "${P_ENV}"/argfile.conf \
+        --tag "kioz0wang/${g_target}:${g_tag}" \
+        -f "${P_ENV}/${g_target}/Containerfile" || return $?
+    podman tag "kioz0wang/${g_target}:${g_tag}" "kioz0wang/${g_target}:latest" || return $?
+
+    if [ -f "${P_ENV}/${g_target}/.post" ]; then
+        bash "${P_ENV}/${g_target}/.post" "${P_ROOT}" || return $?
+    fi
 }
 
 function create() {
-    podman container create --http-proxy=false --interactive --tty --publish 10022:10022 --volume "${P_ROOT}":"/root/${N_ROOT}" --name buildenv localhost/kioz0wang/buildenv:1.0
-}
-
-function run() {
-    podman container start buildenv || return $?
-    echo "=> login root with password [123]"
-    echo "  ssh -o StrictHostKeyChecking=no -p 10022 root@localhost"
-}
-
-function run_attach() {
-    run || return $?
-    podman container attach buildenv
+    if [ $# -lt 1 ]; then
+        echo "Usage: create {name} [ssh_port]"
+        return
+    fi
+    local -r name=$1
+    local -i ssh_port
+    local -a publish
+    if [ $# -gt 1 ]; then
+        ssh_port=$2
+        publish=(--publish "${ssh_port}:10022")
+    fi
+    podman container create --http-proxy=false --interactive --tty \
+        "${publish[@]}" \
+        --volume "${P_ROOT}":"/root/${N_ROOT}" \
+        --name "${name}" \
+        localhost/"kioz0wang/${g_target}:${g_tag}" || return $?
+    if [ -n "${ssh_port}" ]; then
+        echo "=> start and login root with password [123]"
+        echo "  podman start ${name}"
+        echo "  ssh -o StrictHostKeyChecking=no -p ${ssh_port} root@localhost"
+    else
+        echo "=> start and attach"
+        echo "  podman container start --attach ${name}"
+    fi
 }
 
 function run_once() {
-    podman run --rm --http-proxy=false --interactive --tty --publish 10022:10022 --volume "${P_ROOT}":"/root/${N_ROOT}" localhost/kioz0wang/buildenv:1.0
+    local -i ssh_port
+    local -a publish
+    if [ $# -gt 0 ]; then
+        ssh_port=$1
+        publish=(--publish "${ssh_port}:10022")
+    fi
+    podman run --rm --http-proxy=false --interactive --tty \
+        "${publish[@]}" \
+        --volume "${P_ROOT}":"/root/${N_ROOT}" \
+        localhost/"kioz0wang/${g_target}:${g_tag}"
 }
 
 function main() {
-    local -r fn=$1
-    shift
-    ${fn} "$@"
-    echo "Result(${fn}) = $?"
+    if [ $# -lt 3 ]; then
+        echo "Usage: setup.sh {target} {tag} {action} [...]"
+        echo "  build"
+        echo "  create {name} [ssh_port]"
+        echo "  run_once [ssh_port]"
+        return
+    fi
+    g_target=$1
+    g_tag=$2
+    local -r action=$3
+    shift 3
+    ${action} "$@"
+    echo "Result(${action}) = $?"
 }
 
 main "$@"
